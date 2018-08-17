806915152e5dd7302a854481a65498af46ffd260
Britni Alexander <britnialexander@gmail.com>
2018-08-17 13:46:19 -0400

    return has responses in item methods too

--- lib/todoable/item.rb
+++ lib/todoable/item.rb
@@ -8,15 +8,22 @@ module Todoable
     module Item
       def new_item(list_id:, name:)
         item_body = { 'item' => {'name' => name } }
-        request(method: :post, endpoint: "/lists/#{list_id}/items", params: item_body)
+        response = request(method: :post,
+                           endpoint: "/lists/#{list_id}/items",
+                           params: item_body)
+        { 'body' => response.parsed_response, 'code' => response.code }
       end
 
       def finish_item(list_id:, item_id:)
-        request(method: :put, endpoint: "/lists/#{list_id}/items/#{item_id}/finish")
+        response = request(method: :put,
+                           endpoint: "/lists/#{list_id}/items/#{item_id}/finish")
+        { 'body' => response.body, 'code' => response.code }
       end
 
       def delete_item(list_id:, item_id:)
-        request(method: :delete, endpoint: "/lists/#{list_id}/items/#{item_id}")
+        response = request(method: :delete,
+                           endpoint: "/lists/#{list_id}/items/#{item_id}")
+        { 'body' => response.parsed_response, 'code' => response.code }
       end
     end
   end
--- test/todoable/item_test.rb
+++ test/todoable/item_test.rb
@@ -8,23 +8,23 @@ class TodoableListTest < Minitest::Test
                                      password: 'todoable')
     end
     VCR.use_cassette('sample_list') do
-      @list = @client.new_list(name: 'List with new item')
+      @list = @client.new_list(name: 'Sample list')
     end
   end
 
   def test_it_makes_an_item
     setup
     VCR.use_cassette('make_an_item') do
-      response = @client.new_item(list_id: @list['id'], name: 'Workout')
-      assert_equal 201, response.code
+      response = @client.new_item(list_id: @list['body']['id'], name: 'Workoutz')
+      assert_equal 201, response['code']
     end
   end
 
   def test_it_gives_an_error_then_you_try_to_make_an_item_with_nil_name
     setup
     VCR.use_cassette('make_an_item_without_name') do
-      response = @client.new_item(list_id: @list['id'], name: nil)
-      assert_equal 422, response.code
+      response = @client.new_item(list_id: @list['body']['id'], name: nil)
+      assert_equal 422, response['code']
     end
   end
 
@@ -32,7 +32,7 @@ class TodoableListTest < Minitest::Test
     setup
     VCR.use_cassette('make_an_item_without_list') do
       response = @client.new_item(list_id: nil, name: 'Run')
-      assert_equal 404, response.code
+      assert_equal 404, response['code']
     end
   end
 
@@ -42,7 +42,7 @@ class TodoableListTest < Minitest::Test
       err = assert_raises ArgumentError do
         @client.new_item()
       end
-      assert_equal "missing keywords: list_id, name", err.message
+      assert_equal 'missing keywords: list_id, name', err.message
     end
   end
 
@@ -50,37 +50,36 @@ class TodoableListTest < Minitest::Test
   def test_it_marks_an_item_finished
     setup
     VCR.use_cassette('mark_item_finished') do
-      item = @client.new_item(list_id: @list['id'], name: 'Call mom')
-      response = @client.finish_item(list_id: @list['id'], item_id: item['id'])
-      assert_equal 200, response.code
+      item = @client.new_item(list_id: @list['body']['id'], name: 'Call ma')
+      response = @client.finish_item(list_id: @list['body']['id'], item_id: item['body']['id'])
+      assert_equal 200, response['code']
     end
   end
 
   def test_mark_item_finished_returns_an_error_code
     setup
     VCR.use_cassette('mark_item_finished_error_code') do
-      item = @client.new_item(list_id: @list['id'], name: 'Call dad')
-      @client.finish_item(list_id: @list['id'], item_id: item['id'])
-      response = @client.finish_item(list_id: @list['id'], item_id: item['id'])
-      assert_equal 404, response.code
+      item = @client.new_item(list_id: @list['body']['id'], name: 'Call pop')
+      @client.finish_item(list_id: @list['body']['id'], item_id: item['body']['id'])
+      response = @client.finish_item(list_id: @list['body']['id'], item_id: item['body']['id'])
+      assert_equal 404, response['code']
     end
   end
 
   def test_it_deletes_an_item
     setup
     VCR.use_cassette('delete_an_item') do
-      item = @client.new_item(list_id: @list['id'], name: 'Lame todootle doot')
-      response = @client.delete_item(list_id: @list['id'], item_id: item['id'])
-      assert_equal 204, response.code
+      item = @client.new_item(list_id: @list['body']['id'], name: 'Lame todootle doot')
+      response = @client.delete_item(list_id: @list['body']['id'], item_id: item['body']['id'])
+      assert_equal 204, response['code']
     end
   end
 
   def test_it_gives_an_error_if_it_cant_find_item_to_delete
     setup
     VCR.use_cassette('delete_an_item_psych') do
-      item = @client.new_item(list_id: @list['id'], name: 'Lame todootle doot')
-      response = @client.delete_item(list_id: @list['id'], item_id: 3)
-      assert_equal 404, response.code
+      response = @client.delete_item(list_id: @list['body']['id'], item_id: 4)
+      assert_equal 404, response['code']
     end
   end
 end
