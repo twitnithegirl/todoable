1eedb343f101a5a8bc200d969e4463546768d840
Britni Alexander <britnialexander@gmail.com>
2018-08-16 23:37:21 -0400

    refactor tests

---
+++ lib/todoable/item.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'httparty'
+require 'json'
+
+module Todoable
+  class Client
+    module Item
+      def new_item(list_id, name)
+        item_body = { 'item' => {'name' => name } }
+        request(method: :post, endpoint: "/lists/#{list_id}/items", params: item_body)
+      end
+
+      def mark_item_finished(list_id, item_id)
+        request(method: :put, endpoint: "/lists/#{list_id}/#{item_id}/finish"
+      end
+
+      def delete_item(list_id, item_id)
+        request(method: :delete, endpoint: "/lists/#{list_id}/#{item_id}"
+      end
+    end
+  end
+end
--- test/todoable/list_test.rb
+++ test/todoable/list_test.rb
@@ -20,26 +20,26 @@ class TodoableListTest < Minitest::Test
   def test_it_makes_a_list
     setup
     VCR.use_cassette('make_a_list') do
-      response = @client.new_list('Exciting new list')
-      assert_equal 'Exciting new list', response['name']
+      response = @client.new_list('Exciting new todoable')
+      assert_equal 'Exciting new todoable', response['name']
     end
   end
 
   def test_it_finds_a_list
     setup
     VCR.use_cassette('find_a_list') do
-      list = @client.new_list('Awesome list')
+      list = @client.new_list('Doot Doot')
       response = @client.find_list(list['id'])
-      assert_equal 'Awesome list', response['name']
+      assert_equal 'Doot Doot', response['name']
     end
   end
 
   def test_it_updates_a_list
     setup
     VCR.use_cassette('update_a_list') do
-      list = @client.new_list('Neato list')
-      response = @client.update_list(list['id'], 'Not really a neat list')
-      assert_equal 'Not really a neat list updated', response.body
+      list = @client.new_list('Neat todoable')
+      response = @client.update_list(list['id'], 'Not really neat')
+      assert_equal 'Not really neat updated', response.body
       assert_equal 200, response.code
     end
   end
@@ -47,7 +47,7 @@ class TodoableListTest < Minitest::Test
   def test_deleting_a_list
     setup
     VCR.use_cassette('delete_a_list') do
-      list = @client.new_list('Lame list')
+      list = @client.new_list('Lame todoot')
       response = @client.delete_list(list['id'])
       assert_equal 204, response.code
     end
